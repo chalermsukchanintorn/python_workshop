2f8425e81bc83503aec8a386af49333e0e2b6b7c
chalermsukchanintorn <chalermsukchanintorn@gmail.com>
2025-07-06 11:52:49 +0700

    v.2 create python02-07

--- python01.py
+++ python01.py
@@ -1,3 +1,9 @@
-print('Hello...')
+# comment ใน pythonใช้เครื่องหมาย Hash/สี่เหลี่ยม/Sharp (#)
+
+# statment คำสั่ง
+# 1 บรรทัด 1 คำสั่ง จะมี semi-colon ปิดท้ายหรือไม่ก็ได้
+print('Hello...');
 print('Hi...')
-print('Hey...')
+# 1 บรรทัดมากกว่า 1 คำสั่ง จะต้องมี semi-colon ขั้นแต่ละคำสั่ง
+print('Hey...');  print('Wow....');  print('Woo....')
+print('Wee...')
